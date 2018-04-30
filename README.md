08813a2bb93a0c3e0bfb881b3da187f5ba9be602
frzkn <farazrk001@gmail.com>
2018-04-30 18:22:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Installation
+'''
+git clone https://github.com/frzkn/matrix-cli.git && cd matrix-cli
+sudo ./install.sh
+'''
---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/sh
+
+cp ./matrix /usr/local/bin
+chmod +x /usr/local/bin/matrix
+RED='\033[0;32m'
+NC='\033[0m'
+echo -e " ${RED}Successfully installed ! ${NC}"
+echo "Type matrix from anywhere to start"
